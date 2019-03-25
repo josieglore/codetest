073963c48ed22efaedaabe53f6263cdd19c65055
Josie Glore <josieglore@gmail.com>
2019-03-25 13:00:27 -0700

    fix newurl setstate

--- src/client/containers/App.jsx
+++ src/client/containers/App.jsx
@@ -23,7 +23,8 @@ class App extends Component {
     this.getMovies = this.getMovies.bind(this);
     this.getNextFactoid = this.getNextFactoid.bind(this);
     this.getPreviousFactoid = this.getPreviousFactoid.bind(this);
-    this.onImageDrop = this.onImageDrop.bind(this);
+    // this.onImageDrop = this.onImageDrop.bind(this);
+    this.showUploadWidget = this.showUploadWidget.bind(this);
   }
   getMovies() {
     axios.get('http://localhost:3000/movies/')
@@ -70,40 +71,87 @@ class App extends Component {
     })
   }
 
-  onImageDrop(images) {
-    console.log(images)
-    const uploads = images.map(image => {
-      console.log(image)
-      const formData = new FormData();
-      const url =  'https://api.cloudinary.com/v1_1/dvgovtrrs/image/upload';
-      formData.append('file', './IMG_1534.JPG');
-      formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET); 
-      formData.append('api_key', 784128697256257);
-      formData.append('timestamp', (Date.now() / 1000) | 0);
-      const config = {
-        headers: {'X-Requested-With': 'XMLHttpRequest' }
-      };
-      return axios.post(url, formData, config)
-        .then(response => {
-            this.setState({
-                newUrl : response.data.url,
-                // uploadedSuccess: true,
-            })
-            console.log(response.data.url)
-        })
-    })
-  };
+  // onImageDrop(images) {
+  //   console.log(images)
+  //   const uploads = images.map(image => {
+  //     console.log(image)
+  //     const formData = new FormData();
+  //     const url =  'https://api.cloudinary.com/v1_1/dvgovtrrs/image/upload';
+  //     formData.append('file', './IMG_1534.JPG');
+  //     formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET); 
+  //     formData.append('api_key', 784128697256257);
+  //     formData.append('timestamp', (Date.now() / 1000) | 0);
+  //     const config = {
+  //       headers: {'X-Requested-With': 'XMLHttpRequest' }
+  //     };
+  //     return axios.post(url, formData, config)
+  //       .then(response => {
+  //           this.setState({
+  //               newUrl : response.data.url,
+  //               // uploadedSuccess: true,
+  //           })
+  //           console.log(response.data.url)
+  //       })
+  //   })
+  // };
 
+  showUploadWidget() { 
+    cloudinary.openUploadWidget({    
+      cloudName: 'dvgovtrrs',    
+      uploadPreset: 'rmwtwx39',    
+      sources: [        
+        'local',        
+        'url',        
+        'camera',        
+        'image_search',        
+        'facebook',        
+        'dropbox',       
+        'instagram'    
+        ],    
+        googleApiKey: "<image_search_google_api_key>",    showAdvancedOptions: true,    
+        cropping: true,   
+        multiple: false,    
+        defaultSource: "local",    
+        styles: {        
+          palette: {            
+            window: "#FFFFFF",            
+            windowBorder: "#90A0B3",            
+            tabIcon: "#0078FF",            
+            menuIcons: "#5A616A",            
+            textDark: "#000000",            
+            textLight: "#FFFFFF",            
+            link: "#0078FF",            
+            action: "#FF620C",            
+            inactiveTabIcon: "#0E2F5A",            
+            error: "#F44235",            
+            inProgress: "#0078FF",            
+            complete: "#20B832",            
+            sourceBg: "#E4EBF1"        
+          },        
+          fonts: {            
+            default: {                
+              active: true            
+            }        
+          }    
+        }}, (err, info) => {   
+          if (!err) {         
+            console.log("Upload Widget event - ", info);
+            if (info.event === 'success') {
+              this.setState({
+                newUrl: info.info.secure_url
+              }) 
+            } 
+          }  
+        }
+      ); 
+    }
+    
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
     const { title, url, description, factoid } = this.state;
-    let widget = window.cloudinary.createUploadWidget({
-      cloudName: 'dvgovtrrs',
-      uploadPreset: 'rmwtwx39' },
-    (error, result) => { });
     return (
       <div>
         <h1>Movies</h1>
@@ -116,8 +164,7 @@ class App extends Component {
         <button onClick={() => {this.getPreviousFactoid()}}>Previous movie</button>
         <button onClick={() => {this.getNextFactoid()}}>Next Movie</button>
         <NewFactoid 
-          onImageDrop={this.onImageDrop}
-          widget={widget}
+          showUploadWidget={this.showUploadWidget}
         />
       </div>
     );
