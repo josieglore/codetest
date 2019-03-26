55aca216c3a75e0296f1f750e90df5d9d36cdd0b
Josie Glore <josieglore@gmail.com>
2019-03-26 00:42:50 -0700

    fix

--- src/server/server.js
+++ src/server/server.js
@@ -13,10 +13,22 @@ app.use(bodyParser.json());
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.resolve(('../../build'))));
 // }
-// app.use(express.static(path.resolve(__dirname, '../../build')));
+app.use(express.static(__dirname + '/../../build/'));
+// express.static(__dirname + '/../../build/');
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../index.html'));
 });
+
+// app.get('/build/webpack-bundle.js', (req, res) => {
+//   res.header('Content-Type', 'text/javascript');
+//   res.sendFile(path.resolve('../../build/webpack-bundle.js'));
+//  });
+
+//  app.get('/js/materialie.min.js', (req, res) => {
+//   res.header('Content-Type', 'text/javascript');
+//   res.sendFile(path.resolve('../client/js/materialize.min.js'));
+//  });
 
 app.get('/movies/', movieController.getMovies, (req, res) => { 
   return res.status(200).json({ 'movies': res.locals.movies });
