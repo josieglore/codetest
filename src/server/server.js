53c9d3bbc74f08b3769de1dcc94da4e16470946e
Josie Glore <josieglore@gmail.com>
2019-03-25 23:06:55 -0700

    fix server port

--- src/server/server.js
+++ src/server/server.js
@@ -21,7 +21,7 @@ app.delete('/movies/deletemovie/:title', movieController.deleteMovie, (req, res)
   return res.status(200).json({ 'movie deleted': res.locals.deletedMovie });
 });
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) console.log(err);
   else console.log('Server listening on 3000');
 });
