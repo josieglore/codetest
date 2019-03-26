55fc36d150145db474f28df957760f0c74ae7a7e
Josie Glore <josieglore@gmail.com>
2019-03-25 23:34:00 -0700

    fix path.resolve

--- src/server/server.js
+++ src/server/server.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../build'));
+  app.use(express.static(path.resolve(('../../build'))));
 }
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, '../../build', '../../index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../build/index.html'));
 });
 
 app.get('/movies/', movieController.getMovies, (req, res) => { 
