b8da23162fbdec71ba30c681c7d847df51aa021c
Josie Glore <josieglore@gmail.com>
2019-03-26 00:05:14 -0700

    change get route

--- src/server/server.js
+++ src/server/server.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(('../../build/index.html'))));
+  app.use(express.static(path.resolve(('../../build'))));
 }
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../build/index.html'));
 });
 
