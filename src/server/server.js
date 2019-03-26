69263452adcbcfd90545a52ab80bd63889b1a931
Josie Glore <josieglore@gmail.com>
2019-03-26 00:01:28 -0700

    trying something

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(('../../build'))));
+  app.use(express.static(path.resolve(('../../build/index.html'))));
 }
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../build/index.html'));
