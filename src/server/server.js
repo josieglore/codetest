0da8dffd921b01433d409b166bdf493366d79a01
Josie Glore <josieglore@gmail.com>
2019-03-26 00:08:21 -0700

    remove if statement

--- src/server/server.js
+++ src/server/server.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(('../../build'))));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.resolve(('../../build'))));
+// }
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../build/index.html'));
 });
